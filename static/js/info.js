8bef5d7321e2705e110ee290a2c4afaa5b6c641b
Sahil Samel <sahilsamel134@gmail.com>
2021-12-27 12:36:07 +0530

    Added light theme

---
+++ static/js/info.js
@@ -0,0 +1,24 @@
+const body = document.querySelector("body");
+const wrapper = document.querySelector(".wrapper");
+const goback = document.querySelector(".goback");
+const footer = document.querySelector("footer");
+
+window.onload = function () {
+    prevmode = localStorage.getItem("theme");
+
+		if(!prevmode)
+		{
+			localStorage.setItem("theme", "dark");
+			prevmode = localStorage.getItem("theme");
+		}
+
+		else if(prevmode === "light")
+			changetheme();
+}
+
+function changetheme(){
+    wrapper.classList.toggle("wrapper-light");
+    body.classList.toggle("body-light");
+    footer.classList.toggle("footer-light");
+    goback.classList.toggle("goback-light");
+}
--- static/js/script.js
+++ static/js/script.js
@@ -1,15 +1,21 @@
-const buttons = document.querySelectorAll(".input");
-const input = document.querySelector(".inputarea");
+const input = document.querySelectorAll(".input");
+const inputarea = document.querySelector(".inputarea");
 const resultarea = document.querySelector(".resultarea");
 const result = document.querySelector(".equalto");
 const clear = document.querySelector(".clear");
 const theme = document.querySelector(".theme");
 const error = document.querySelector(".error");
+const buttons =document.querySelectorAll(".buttons");
+const body = document.querySelector("body");
+const darkquestion = document.getElementById("dark");
+const lightquestion = document.getElementById("light");
 
 
 var noinput = document.createTextNode('Please enter something first');
 var invinput = document.createTextNode('Invalid input');
 
+
+let prevmode;
 
 function append(exp,value){
     exp += value;
@@ -22,9 +28,9 @@ function removeAllChildNodes(parent) {
     }
 }
 
-for(let i=0; i < buttons.length; i++){
-    buttons[i].onclick = function(){
-        input.value = append(input.value,buttons[i].innerText).trim();
+for(let i=0; i < input.length; i++){
+    input[i].onclick = function(){
+        inputarea.value = append(inputarea.value,input[i].innerText).trim();
     }
 }
 
@@ -43,7 +49,7 @@ function solve(input){
 }
 
 result.onclick = function(){   
-    var ans = solve(input.value);
+    var ans = solve(inputarea.value);
     if (ans != undefined) {
         resultarea.value = ans;
     }
@@ -56,12 +62,53 @@ String.prototype.removelast = function (i) {
 }
 
 clear.onclick = function(){
-    let string = input.value;
-    input.value=string.removelast(-1);
+    let string = inputarea.value;
+    inputarea.value=string.removelast(-1);
 }
 
 clear.addEventListener('long-press', function(e) {
-    input.value="";
+    inputarea.value="";
     resultarea.value="";
     removeAllChildNodes(error);
 });
+
+window.onload = function () {
+    prevmode = localStorage.getItem("theme");
+
+		if(!prevmode)
+		{
+			localStorage.setItem("theme", "dark");
+			prevmode = localStorage.getItem("theme");
+		}
+
+		else if(prevmode === "light")
+			changetheme();
+}
+
+theme.onclick = function(){
+    if (prevmode === "dark") {
+        localStorage.setItem("theme", "light");
+        prevmode = "light";
+        changetheme();
+    }
+
+    else if (prevmode === "light") {
+        localStorage.setItem("theme", "dark");
+        prevmode = "dark";
+        changetheme();
+    }
+}
+
+function changetheme(){
+inputarea.classList.toggle("inputarea-light");
+resultarea.classList.toggle("resultarea-light");
+body.classList.toggle("body-light");
+error.classList.toggle("error-light");
+lightquestion.classList.toggle("hide");
+darkquestion.classList.toggle("hide");
+
+
+for(let i = 0; i < buttons.length; i++)
+	buttons[i].classList.toggle("buttons-light");
+}
+
